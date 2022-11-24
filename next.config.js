e9bf8b621ba5e0c813d8bc7f9bc8cae103f17251
Kalon Robson <kalon.robson@innovixx.co.uk>
2022-11-24 22:13:24 +0000

    feat: rm NEXT_PUBLIC_SERVER_URL env

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { sizes } = require("./src/blocks/Image/sizes");
 
 module.exports = {
   publicRuntimeConfig: {
-    SERVER_URL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
+    SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
   },
   images: {
     domains: [
--- payload.config.ts
+++ payload.config.ts
@@ -1,10 +1,10 @@
-import { buildConfig } from 'payload/config';
 import dotenv from 'dotenv';
+import { buildConfig } from 'payload/config';
 import { Media, Page } from './cms/collections';
 
 dotenv.config();
 
 export default buildConfig({
-  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL,
   collections: [Page, Media],
 });
